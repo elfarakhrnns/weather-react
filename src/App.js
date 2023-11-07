9550c56d254117a53b26e120db47928f20ec3925
elfarakhrnns <elfara.khrnns@gmail.com>
2023-11-07 11:17:08 +0700

    adding new react server

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "./logo.svg";
+import Weather from "./Weather";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World! 👋🏻</h1>
+        <Weather />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Weather.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Weather() {
+  return (
+    <div className="App">
+      <header className="app-header">
+        <h2>This is the weather</h2>
+      </header>
+    </div>
+  );
+}
